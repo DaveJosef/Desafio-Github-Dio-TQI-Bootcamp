6b532624601a0cef0685bc31bef18fc6470d195a
DaveJosef <josedaaaaavid@gmail.com>
2022-08-09 11:44:55 -0300

    Adiciona images em naruto-quotes-client

--- Cursos/ReactJS/naruto-quotes-client/src/pages/app/App.js
+++ Cursos/ReactJS/naruto-quotes-client/src/pages/app/App.js
@@ -1,6 +1,15 @@
+import styled from "styled-components";
+import narutoImg from '../../images/naruto.png';
 
 export function App() {
   return (
-    <div>App</div>
+    <Content>
+      <div>App</div>
+      <NarutoImg alt="Naruto holding a kunai" src={narutoImg} />
+    </Content>
   );
 }
+
+const Content = styled.div``;
+
+const NarutoImg = styled.img``;
