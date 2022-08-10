dbff10b5a006d95b57203423901aa87ae31e7209
DaveJosef <josedaaaaavid@gmail.com>
2022-08-10 13:58:32 -0300

    Adiciona aula-hooks

---
+++ Cursos/ReactJS/aula-hooks/src/Components/App/App.js
@@ -0,0 +1,15 @@
+import Counter from '../Counter/Counter';
+import IfoodCounter from '../IfoodCounter/IfoodCounter';
+import SmartCounter from '../SmartCounter/SmartCounter';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <IfoodCounter />
+    </>
+  );
+}
+
+export default App;
