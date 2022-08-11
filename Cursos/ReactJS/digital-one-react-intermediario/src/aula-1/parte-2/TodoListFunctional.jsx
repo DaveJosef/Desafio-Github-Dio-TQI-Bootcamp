c3d72c62cfbdbfb73f47f44aa3c1d06edd08d49f
DaveJosef <josedaaaaavid@gmail.com>
2022-08-11 13:42:42 -0300

    Adiciona parte-2 em digital-one-react-intermediario > aula-1

--- Cursos/ReactJS/digital-one-react-intermediario/src/aula-1/parte-2/TodoListFunctional.jsx
+++ Cursos/ReactJS/digital-one-react-intermediario/src/aula-1/parte-2/TodoListFunctional.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TodoListFunctional = () => {
+  const [items, setItems] = useState(['Tomate', 'Alface', 'Melancia']);
+
+  const addItem = (item) => {
+    setItems([...items, item]);
+  }
+
+  const removeItem = () => {
+    setItems([...items.slice(1)]);
+  }
+
   return (
-    <div>TodoListFunctional</div>
+    <div className='bloco-lista'>
+      <p>Minha lista</p>
+      <ul className='lista-estilizada'>
+        {items.map(item => <li>{item}</li>)}
+      </ul>
+      <button onClick={() => addItem('Abóbora')}>Add Item</button>
+      <button onClick={() => removeItem()}>Remove Item</button>
+    </div>
   )
 }
 
