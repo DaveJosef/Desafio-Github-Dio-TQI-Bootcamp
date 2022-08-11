e2ca10b36c31e8d8211e579711122d2250a524f4
DaveJosef <josedaaaaavid@gmail.com>
2022-08-11 12:13:14 -0300

    Altera parte-1 em digital-one-react-intermediario > aula-1

--- Cursos/ReactJS/digital-one-react-intermediario/src/aula-1/parte-1/Estilos.jsx
+++ Cursos/ReactJS/digital-one-react-intermediario/src/aula-1/parte-1/Estilos.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import CLassName from './componentes/CLassName';
+import Inline from './componentes/Inline';
+import Styled from './componentes/Styled';
 
-const Estilos = () => {
-  return (
-    <div>Estilos</div>
-  )
-}
+const Estilos = () => (
+    <div style={{display: 'flex', justifyContent: 'space-around'}}>
+      <Inline />
+      <CLassName />
+      <Styled />
+    </div>
+);
 
 export default Estilos;
