af0b11e51c2457d78f13cd233216c5ea6c3e2d97
DaveJosef <josedaaaaavid@gmail.com>
2022-08-13 11:21:26 -0300

    Altera aula-3 em digital-one-react-intermediario

--- Cursos/ReactJS/digital-one-react-intermediario/src/redux/reducers/rootReducer.js
+++ Cursos/ReactJS/digital-one-react-intermediario/src/redux/reducers/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
+import { cientistasReducer } from "./cientistas";
 import { counterReducer } from './counter';
 
 const rootReducer = combineReducers({
+    cientistas: cientistasReducer,
     counter: counterReducer,
 });
 
