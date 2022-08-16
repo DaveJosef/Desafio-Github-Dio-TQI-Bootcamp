b97724a61dce2ec92f086b83ec95b1be2a2c6f8d
DaveJosef <josedaaaaavid@gmail.com>
2022-08-16 11:54:46 -0300

    Adiciona advanced-reactjs-dio > smart-components

---
+++ Cursos/ReactJS/advanced-reactjs-dio/smart-components/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Button from './Component/Button';
+import Calc from './Component/Calc';
+import PhotosGallery from './Component/PhotosGallery';
+
+export default function App() {
+
+  const photos = [
+    'http://placeimg.com/140/60/people',
+    'http://placeimg.com/140/60/animals',
+    'http://placeimg.com/140/60/tech',
+    'http://placeimg.com/140/60/any',
+    'http://placeimg.com/140/60/nature',
+  ];
+
+  return (
+    <>
+      <h1>Galeria de fotos</h1>
+      <PhotosGallery photos={photos} />
+    </>
+  )
+}
