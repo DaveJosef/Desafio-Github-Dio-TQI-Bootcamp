bbc36c473ae4e5050f9293eed7c900e6e9ff04cf
DaveJosef <josedaaaaavid@gmail.com>
2022-08-29 12:24:19 -0300

    Adiciona estruturaDeDadosJavaDio

---
+++ Cursos/Java/estruturaDeDadosJavaDio/projetos/Aula1/Etapa3/primeiroProjeto/src/main/java/com/estruturadedados/Main.java
@@ -0,0 +1,8 @@
+package com.estruturadedados;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Bem vindo ao curso de Estrutura de Dados em Java!");
+    }
+}
