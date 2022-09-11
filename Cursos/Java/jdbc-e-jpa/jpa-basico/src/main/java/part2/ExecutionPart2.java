ef009e1edf406b90deffa6b3ac185032ef98a8cf
DaveJosef <josedaaaaavid@gmail.com>
2022-09-11 13:34:31 -0300

    Adiciona ExecutionPart2

--- Cursos/Java/jdbc-e-jpa/jpa-basico/src/main/java/part2/ExecutionPart2.java
+++ Cursos/Java/jdbc-e-jpa/jpa-basico/src/main/java/part2/ExecutionPart2.java
@@ -1,4 +1,71 @@
 package part2;
 
+import classes.Aluno;
+import classes.Estado;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class ExecutionPart2 {
+
+    public static void main(String[] args) {
+
+        /*
+            1. Criar um gerenciador de entidades
+         */
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("part2-DIO");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        /*
+            2.1 Criar instâncias para persistir no Banco
+         */
+        Estado estadoParaAdd = new Estado("Rio de Janeiro", "RJ");
+        Aluno alunoParaAdd = new Aluno("David", 29, estadoParaAdd);
+
+        /*
+            2.2 Persistir instâncias com transação
+         */
+        entityManager.getTransaction().begin();
+
+        entityManager.persist(estadoParaAdd);
+        entityManager.persist(alunoParaAdd);
+
+        entityManager.getTransaction().commit();
+
+        /*
+            3. Resgatar instâncias com transação
+         */
+//        Estado estadoEncontrado = entityManager.find(Estado.class, 1);
+//        Aluno alunoEncontrado = entityManager.find(Aluno.class, 1);
+//
+//        System.out.println(estadoEncontrado);
+//        System.out.println(alunoEncontrado);
+//
+        /*
+            4. Alterar instâncias com transação
+         */
+//        entityManager.getTransaction().begin();
+//
+//        alunoEncontrado.setNome("Karam");
+//        alunoEncontrado.setIdade(20);
+//
+//        entityManager.getTransaction().commit();
+
+        /*
+            5. Remover instâncias com transação
+         */
+//        entityManager.getTransaction().begin();
+//
+//        entityManager.remove(alunoEncontrado);
+//
+//        entityManager.getTransaction().commit();
+
+        /*
+            Fechar entityManagerFactory e entityManager
+         */
+        entityManager.close();
+        entityManagerFactory.close();
+
+    }
 }
--- Cursos/Java/jdbc-e-jpa/jpa-basico/src/main/java/part3/ExecutionPart3.java
+++ Cursos/Java/jdbc-e-jpa/jpa-basico/src/main/java/part3/ExecutionPart3.java
@@ -1,9 +1,36 @@
 package part3;
 
 
+import classes.Aluno;
+import classes.Estado;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class ExecutionPart3 {
 
     public static void main(String[] args) {
+
+        /*
+            1. Criar um gerenciador de entidades
+         */
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("part2-DIO");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        /*
+            2.1 Criar instâncias para persistir no Banco
+         */
+        entityManager.getTransaction().begin();
+
+        Estado estadoParaAdd = new Estado("Rio de Janeiro", "RJ");
+        entityManager.persist(estadoParaAdd);
+        entityManager.persist(new Estado("Sao Paulo", "SP"));
+        entityManager.persist(new Aluno("Daniel", 29, estadoParaAdd));
+        entityManager.persist(new Aluno("Joao", 20, estadoParaAdd));
+        entityManager.persist(new Aluno("David", 29, estadoParaAdd));
+
+        entityManager.getTransaction().commit();
 
     }
 }
