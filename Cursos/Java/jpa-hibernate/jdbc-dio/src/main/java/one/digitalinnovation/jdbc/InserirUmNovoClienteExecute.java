85f4f6a9d68aae30a6bb286dcb6a26670f065990
DaveJosef <josedaaaaavid@gmail.com>
2022-09-14 11:32:31 -0300

    Adiciona jpa-hibernate > BuscarTodosClientesExecute

---
+++ Cursos/Java/jpa-hibernate/jdbc-dio/src/main/java/one/digitalinnovation/jdbc/InserirUmNovoClienteExecute.java
@@ -0,0 +1,17 @@
+package one.digitalinnovation.jdbc;
+
+import one.digitalinnovation.jdbc.configuration.DB;
+import one.digitalinnovation.jdbc.dao.ClienteDAO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class InserirUmNovoClienteExecute {
+
+    public static void main(String[] args) throws SQLException {
+        try (Connection connection = DB.getConnection()) {
+            ClienteDAO clienteDAO = new ClienteDAO(connection);
+            clienteDAO.inserirNovoCliente("Joao");
+        }
+    }
+}
