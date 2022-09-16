2d891b65d9fb87d282dcba2256c66b399b44c17f
DaveJosef <josedaaaaavid@gmail.com>
2022-09-16 11:33:22 -0300

    Altera Multa em jpa-hibernate > jpa-dio

--- Cursos/Java/jpa-hibernate/jpa-dio/src/main/java/com/digitalinnovationone/jpa/model/Cliente.java
+++ Cursos/Java/jpa-hibernate/jpa-dio/src/main/java/com/digitalinnovationone/jpa/model/Cliente.java
@@ -1,4 +1,65 @@
 package com.digitalinnovationone.jpa.model;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_cliente")
 public class Cliente {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Carro> carrros;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Carro> getCarrros() {
+        return carrros;
+    }
+
+    public void setCarrros(List<Carro> carrros) {
+        this.carrros = carrros;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        if (id == null) {
+            if (cliente.id != null)
+                return false;
+        } else if (!id.equals(cliente.id))
+            return false;
+        return true;
+    }
 }
--- Cursos/Java/jpa-hibernate/jpa-dio/src/main/java/com/digitalinnovationone/jpa/model/Multa.java
+++ Cursos/Java/jpa-hibernate/jpa-dio/src/main/java/com/digitalinnovationone/jpa/model/Multa.java
@@ -1,4 +1,56 @@
 package com.digitalinnovationone.jpa.model;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_multa")
 public class Multa {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "valor", nullable = false)
+    private Double valor;
+
+    @Column(name = "data")
+    private LocalDateTime data;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "carro_id", nullable = false)
+    private Carro carro;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public Carro getCarro() {
+        return carro;
+    }
+
+    public void setCarro(Carro carro) {
+        this.carro = carro;
+    }
 }
