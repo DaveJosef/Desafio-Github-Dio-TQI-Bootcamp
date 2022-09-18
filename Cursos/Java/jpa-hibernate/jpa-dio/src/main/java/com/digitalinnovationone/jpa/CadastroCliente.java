fb28ceec035dc6582fdf1decefede10c7157a5b7
DaveJosef <josedaaaaavid@gmail.com>
2022-09-18 11:48:07 -0300

    Adiciona CadastroCliente em jpa-hibernate > jpa-dio

--- Cursos/Java/jpa-hibernate/jpa-dio/src/main/java/com/digitalinnovationone/jpa/CadastroCliente.java
+++ Cursos/Java/jpa-hibernate/jpa-dio/src/main/java/com/digitalinnovationone/jpa/CadastroCliente.java
@@ -1,28 +1,22 @@
 package com.digitalinnovationone.jpa;
 
-import com.digitalinnovationone.jpa.model.Carro;
 import com.digitalinnovationone.jpa.model.Cliente;
-import com.digitalinnovationone.jpa.model.Multa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.time.LocalDateTime;
 
-public class CadastroClienteTestarTransacao {
+public class CadastroCliente {
 
     public static void main(String[] args) {
 
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Clientes-PU");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
 
-        Cliente cliente = new Cliente();
-        cliente.setNome("Joao Paulo");
-        entityManager.persist(cliente);
-
+        Cliente cliente = entityManager.find(Cliente.class, 1);
         entityManager.getTransaction().commit();
         entityManager.close();
         entityManagerFactory.close();
+        System.out.println(cliente.getCarrros().get(0).getMarca());
     }
 }
