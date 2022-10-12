7fc0d7236fa1806ca5da5b6d1746c221355a90cc
DaveJosef <josedaaaaavid@gmail.com>
2022-10-12 12:27:19 -0300

    Adiciona dio-springboot > spring-ioc-di-beans-autowired

---
+++ Cursos/Spring/dio-springboot/spring-ioc-di-beans-autowired/src/test/java/dio/springboot/SpringPrimeirosPassosApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringPrimeirosPassosApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
