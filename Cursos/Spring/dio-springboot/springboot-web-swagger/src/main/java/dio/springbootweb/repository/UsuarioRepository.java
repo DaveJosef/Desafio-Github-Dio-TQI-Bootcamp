ac55d49b9bff54618fbf6462e4d17fe0f1714dca
DaveJosef <josedaaaaavid@gmail.com>
2022-10-14 12:06:58 -0300

    Adiciona dio-springboot > springboot-web-swagger

---
+++ Cursos/Spring/dio-springboot/springboot-web-swagger/src/main/java/dio/springbootweb/repository/UsuarioRepository.java
@@ -0,0 +1,39 @@
+package dio.springbootweb.repository;
+
+import dio.springbootweb.model.Usuario;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UsuarioRepository {
+
+    public void save(Usuario usuario) {
+        System.out.println("SAVE - Recebendo o usuário na camada de repositório");
+        System.out.println(usuario);
+    }
+
+    public void update(Usuario usuario) {
+        System.out.println("UPDATE - Recebendo o usuário na camada de repositório");
+        System.out.println(usuario);
+    }
+
+    public void remove(Integer id) {
+        System.out.println(String.format("DELETE/id - Recebendo o id: %d para excluir um usuário", id));
+        System.out.println(id);
+    }
+
+    public List<Usuario> listAll() {
+        ArrayList<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(new Usuario("david", "password"));
+        usuarios.add(new Usuario("frank", "masterpass"));
+        return usuarios;
+    }
+
+    public Usuario findById(Integer id) {
+        System.out.println(String.format("GET/id - Recebendo o id: %d para localizar um usuário", id));
+        return new Usuario("david", "password");
+    }
+
+}
